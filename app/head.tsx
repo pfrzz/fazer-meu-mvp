77d92bd37e70033cd774f4442496476ccc827d8a
Pedro Ferraz <111819887+pfrzz@users.noreply.github.com>
2025-06-05 13:50:15 -0300

    Create head.tsx

---
+++ app/head.tsx
@@ -0,0 +1,12 @@
+// app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <title>Fazer Meu MVP | Criação Ágil de MVPs para Startups</title>
+      <meta
+        name="description"
+        content="Transforme sua ideia em um MVP funcional. Desenvolvemos sites e aplicativos para validar seu negócio de forma rápida e eficiente."
+      />
+    </>
+  );
+}
